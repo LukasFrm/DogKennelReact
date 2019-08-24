23f63b8b5403f2868d1228fca339b5f7860ef1a1
LukasFrm <lukas.freimonas@gmail.com>
2019-08-24 20:52:47 +0300

    Updates

---
+++ precache-manifest.227b752b1326ab2c06487c7ea303fa39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "17f931f32555596c9bcee93b4e6423d4",
+    "url": "/DogKennelReact-Build/static/media/flag_uk.17f931f3.JPG"
+  },
+  {
+    "revision": "fc3c9bb2db5a72a44e9d50b6567a5be2",
+    "url": "/DogKennelReact-Build/static/media/Beagles.fc3c9bb2.png"
+  },
+  {
+    "revision": "b3be28088911ef917dbf",
+    "url": "/DogKennelReact-Build/static/js/runtime~main.40a41dd5.js"
+  },
+  {
+    "revision": "79613cf222740af94d5a",
+    "url": "/DogKennelReact-Build/static/js/main.66bede63.chunk.js"
+  },
+  {
+    "revision": "29aecb9cf06678e9b179",
+    "url": "/DogKennelReact-Build/static/js/2.8283024e.chunk.js"
+  },
+  {
+    "revision": "79613cf222740af94d5a",
+    "url": "/DogKennelReact-Build/static/css/main.80956e62.chunk.css"
+  },
+  {
+    "revision": "29aecb9cf06678e9b179",
+    "url": "/DogKennelReact-Build/static/css/2.93bf6588.chunk.css"
+  },
+  {
+    "revision": "ca5d41189095fa8cb3f43afa9b4e725c",
+    "url": "/DogKennelReact-Build/index.html"
+  }
+];
